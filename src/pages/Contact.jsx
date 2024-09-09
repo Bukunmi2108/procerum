dea1af5659a32f7e6d548f73fe084da392716104
Bukunmi2108 <bkakinyemi21@gmail.com>
2024-09-09 09:23:35 -0700

    Added functionality to the click to call btn

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,21 @@ import {Navbar, Footer} from '../containers'
 import {Form} from '../containers'
 
 const Contact = () => {
+  const handleSMSClick = () => {
+    const recipient = '+2349137099774'
+    const message = 'I would to reach out'
+
+    const smsUrl = `sms:${recipient}?body=${encodeURIComponent(message)}`
+    window.location.href = smsUrl
+  }
+
+  const handleTELClick = () => {
+    const phonenumber = '+2349137099774'
+
+    const telUrl = `tel:${phonenumber}`
+    window.location.href = telUrl
+  }
+
   return (
     <div>
         <Navbar />
@@ -10,8 +25,8 @@ const Contact = () => {
             <div className='flex flex-col gap-2 justify-center pt-12'>
                 <h1 className='text-4xl text-primary font-semibold'>Contact Us Today</h1>
                 <div className='flex flex-row gap-2'>
-                    <p className='cursor-pointer p-4 border-purple-500 border-2 w-fit hover:text-white hover:bg-purple-500 rounded'>Click to sms</p>
-                    <p className='cursor-pointer p-4 border-purple-500 border-2 w-fit hover:text-white hover:bg-purple-500 rounded'>Click to call</p>
+                    <p onClick={handleSMSClick} className='cursor-pointer p-4 border-purple-500 border-2 w-fit hover:text-white hover:bg-purple-500 rounded'>Click to sms</p>
+                    <p onClick={handleTELClick} className='cursor-pointer p-4 border-purple-500 border-2 w-fit hover:text-white hover:bg-purple-500 rounded'>Click to call</p>
                 </div>
                 <p>Feel free to send us a message</p>
             </div>
